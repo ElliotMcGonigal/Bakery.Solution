182507c68a743d5300d269ddd01cfe822230598f
Elliot McGonigal <elliot@elliotmcgonigal.com>
2022-07-02 23:12:59 -0700

    add rest of console front end

--- Bakery/Models/Program.cs
+++ Bakery/Models/Program.cs
@@ -7,10 +7,15 @@ namespace Pierres
   {
     public static void Main()
     {
+      Console.WriteLine("Welcome to Pierre's Bakery! Here we only sell loaves of bread and pastries. Each loaf is $5, but we have a buy 2 get one free sale! Each pastry is $2, but we have a 3 pastries for $5 deal!");
       Console.WriteLine("How many loaves of BREAD would you like?(enter '0' for none, please enter only a number)");
       int loavesOfBread = int.Parse(Console.ReadLine());
       Console.WriteLine("How many PASTRIES would you like?(enter '0' for none, please enter only a number)");
       int numberOfPastries = int.Parse(Console.ReadLine());
+      Bread bread = new Bread();
+      Pastry pastry = new Pastry();
+      int totalPrice = bread.BreadCost(loavesOfBread) + pastry.PastryCost(numberOfPastries);
+      Console.WriteLine("Your total cost will be $" + totalPrice + "! Enjoy your bakery bounty!");
     }
   }
 }
